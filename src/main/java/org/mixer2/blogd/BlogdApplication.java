d8fe06e96258376c5089c5c2e74847387b17d94c
Watanabe <nabedge@users.noreply.github.com>
2015-12-24 10:45:20 +0900

    first commit.

---
+++ src/main/java/org/mixer2/blogd/BlogdApplication.java
@@ -0,0 +1,12 @@
+package org.mixer2.blogd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogdApplication.class, args);
+    }
+}
